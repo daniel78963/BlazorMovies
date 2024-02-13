ef39000f84ffd0e8b333aab532d3ef5a2d6b386f
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-02-13 18:04:33 -0500

    add services

---
+++ BlazorMovies/BlazorMovies/Client/Services.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorMovies.Client
+{
+    public class ServicesSingleton
+    {
+        public int Value { get; set; }
+    }
+
+    public class ServicesTransient
+    {
+        public int Value { get; set; }
+    }
+}
