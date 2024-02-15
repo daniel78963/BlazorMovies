e80a5b382e5881f5e13771dfc9e3a4a9ea8a1233
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-02-14 21:31:15 -0500

    partial class

--- BlazorMovies/BlazorMovies/Client/Pages/Counter.razor.cs
+++ BlazorMovies/BlazorMovies/Client/Pages/Counter.razor.cs
@@ -1,6 +1,19 @@
-﻿namespace BlazorMovies.Client.Pages
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorMovies.Client.Pages
 {
     public partial class Counter
     {
+        [Inject] ServicesSingleton singleton { get; set; } = null!;
+        [Inject] ServicesTransient transient { get; set; } = null!;
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleton.Value = currentCount;
+            transient.Value = currentCount;
+        }
     }
 }
