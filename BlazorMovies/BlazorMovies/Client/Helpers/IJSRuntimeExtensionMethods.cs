485d9e0ac1e16b41ee1d839cc6363fd9e97a7955
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-02-16 21:43:45 -0500

    IJSRuntimeExtensionMethods

---
+++ BlazorMovies/BlazorMovies/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using BlazorMovies.Shared.Entities;
+using Microsoft.JSInterop;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            return await js.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
