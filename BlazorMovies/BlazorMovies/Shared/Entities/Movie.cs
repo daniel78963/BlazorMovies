d27fb5c341e4aedc82b3a060f05d4383df6cc870
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-02-19 21:43:30 -0500

    TitleShort

--- BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
+++ BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
@@ -2,8 +2,26 @@
 {
     public class Movie
     {
-        //public string Title { get; set; } = null!;
-        public string? Title { get; set; }  
+        public string? Title { get; set; }
         public DateTime DateLaunch { get; set; }
+        public string Poster { get; set; } = null!;
+        public string? TitleShort
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Title))
+                {
+                    return null;
+                }
+                if (Title.Length > 60)
+                {
+                    return Title.Substring(0, 60) + "...";
+                }
+                else
+                {
+                    return Title;
+                }
+            }
+        }
     }
 }
