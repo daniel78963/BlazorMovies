1c17b3707f931d76ab48ed68f4913df64318d28b
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-02-20 21:23:23 -0500

    porters

--- BlazorMovies/BlazorMovies/Client/Repositories/Repository.cs
+++ BlazorMovies/BlazorMovies/Client/Repositories/Repository.cs
@@ -8,9 +8,15 @@ namespace BlazorMovies.Client.Repositories
         {
             return new List<Movie>()
             {
-            new Movie() { Title = "Black, Amor y Compasión", DateLaunch = new DateTime(2023, 11, 11) },
-            new Movie() { Title = "Winter", DateLaunch = new DateTime(2024, 05, 19) },
-            new Movie() { Title = "Panter", DateLaunch = new DateTime(2024, 1, 21) }
+            new Movie() { Title = "Black, Amor y Compasión", 
+                DateLaunch = new DateTime(2023, 11, 11), 
+                Poster = "https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Black_Film.jpg/220px-Black_Film.jpg" },
+            new Movie() { Title = "Terminator", 
+                DateLaunch = new DateTime(2024, 05, 19),
+                Poster = "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Terminator1984movieposter.jpg/220px-Terminator1984movieposter.jpg" },
+            new Movie() { Title = "Dune", 
+                DateLaunch = new DateTime(2024, 1, 21),
+                Poster = "https://upload.wikimedia.org/wikipedia/en/thumb/8/8e/Dune_%282021_film%29.jpg/220px-Dune_%282021_film%29.jpg" }
             };
         }
     }
