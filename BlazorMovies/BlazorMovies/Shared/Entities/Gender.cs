c18db21c3bd4064778f129d85dd6e692b341c5a5
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-02-24 12:30:57 -0500

    form Gender

--- BlazorMovies/BlazorMovies/Shared/Entities/Gender.cs
+++ BlazorMovies/BlazorMovies/Shared/Entities/Gender.cs
@@ -1,8 +1,12 @@
-﻿namespace BlazorMovies.Shared.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMovies.Shared.Entities
 {
     public class Gender
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required")]
         public string Name { get; set; }
     }
 }
