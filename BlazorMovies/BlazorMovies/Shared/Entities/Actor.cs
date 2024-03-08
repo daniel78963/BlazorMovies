aa746dadd59c096fe21249787adc16ce6b657f13
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-03-07 21:48:42 -0500

    Actor

---
+++ BlazorMovies/BlazorMovies/Shared/Entities/Actor.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorMovies.Shared.Entities
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Biography { get; set; }
+        public string? Photo { get; set; }
+        public DateTime? DateBirth { get; set; }
+    }
+}
