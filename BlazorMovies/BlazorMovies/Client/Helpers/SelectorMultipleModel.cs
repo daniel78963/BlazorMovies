ede1c3a1856d377d32e2c6d8f49a57791990f07c
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-03-12 16:12:46 -0500

    selector-multiple

---
+++ BlazorMovies/BlazorMovies/Client/Helpers/SelectorMultipleModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorMovies.Client.Helpers
+{
+    public class SelectorMultipleModel
+    {
+        public SelectorMultipleModel(string key, string value)
+        {
+            Value = value;
+            Key = key;
+        }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
