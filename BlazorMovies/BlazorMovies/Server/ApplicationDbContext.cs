0b1b8954d0dbadec21ea71327cbac33189be1386
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-04-02 17:01:00 -0500

    ConnectionStrings

--- BlazorMovies/BlazorMovies/Server/ApplicationDbContext.cs
+++ BlazorMovies/BlazorMovies/Server/ApplicationDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace BlazorMovies.Server
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorMovies.Server
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            //estamos pasando que parametros o condiciones vamos a usar para nuestros repositorios, ej: db de que tipo
+        }
     }
 }
