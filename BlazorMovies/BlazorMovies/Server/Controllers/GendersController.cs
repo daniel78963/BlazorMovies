e882289a9cc6af0e7c83290eade542c44b9a458b
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-04-09 21:29:14 -0500

    GendersController

---
+++ BlazorMovies/BlazorMovies/Server/Controllers/GendersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorMovies.Server.Controllers
+{
+    public class GendersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
