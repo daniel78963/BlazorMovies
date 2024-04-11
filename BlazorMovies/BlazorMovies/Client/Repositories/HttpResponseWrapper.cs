184d60d2e303d6faaaad79efbe92498853914e3c
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-04-10 21:23:59 -0500

    HttpResponseWrapper T

--- BlazorMovies/BlazorMovies/Client/Repositories/HttpResponseWrapper.cs
+++ BlazorMovies/BlazorMovies/Client/Repositories/HttpResponseWrapper.cs
@@ -2,5 +2,15 @@
 {
     public class HttpResponseWrapper<T>
     {
+        public HttpResponseWrapper(T response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public bool Error { get; set; }
+        public T Response { get; set; }
+        public HttpResponseMessage HttpResponseMessage { get; set; }
     }
 }
