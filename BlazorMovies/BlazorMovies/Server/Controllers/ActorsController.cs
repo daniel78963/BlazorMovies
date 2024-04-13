533a2e1fc06729e41a714464da35faa4f452d1c0
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-04-13 10:36:20 -0500

    ActorsController

---
+++ BlazorMovies/BlazorMovies/Server/Controllers/ActorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorMovies.Server.Controllers
+{
+    [Route("api/actors")]
+    [ApiController]
+    public class ActorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ActorsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
