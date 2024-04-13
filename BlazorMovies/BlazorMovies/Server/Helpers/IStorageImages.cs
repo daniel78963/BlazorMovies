a14f4f205db1daa15ab267b29a8870613c4e1da8
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-04-13 11:46:44 -0500

    IStorageImages signatures

--- BlazorMovies/BlazorMovies/Server/Helpers/IStorageImages.cs
+++ BlazorMovies/BlazorMovies/Server/Helpers/IStorageImages.cs
@@ -2,5 +2,8 @@
 {
     public interface IStorageImages
     {
+        Task<string> SaveFile(byte[] content, string extension, string nameContainer);
+        Task DeleteFile(string path, string nameContainer);
+        Task<string> EditFile(byte[] content, string extension, string nameContainer, string path);
     }
 }
