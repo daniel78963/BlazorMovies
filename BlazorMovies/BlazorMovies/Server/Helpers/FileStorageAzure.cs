9caf4ac9e6d952d3fbaa24dda5988dd373b90edb
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-04-15 18:46:29 -0500

    connectionString

--- BlazorMovies/BlazorMovies/Server/Helpers/FileStorageAzure.cs
+++ BlazorMovies/BlazorMovies/Server/Helpers/FileStorageAzure.cs
@@ -3,6 +3,12 @@ namespace BlazorMovies.Server.Helpers
 {
     public class FileStorageAzure : IStorageFiles
     {
+        private string connectionString;
+        public FileStorageAzure(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("AzureStorage")!;
+        }
+
         public Task DeleteFile(string path, string nameContainer)
         {
             throw new NotImplementedException();
