05a104e9bcdb308d5470631d57835852b8aac2f7
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-04-15 18:44:47 -0500

    FileStorageAzure

---
+++ BlazorMovies/BlazorMovies/Server/Helpers/FileStorageAzure.cs
@@ -0,0 +1,16 @@
+﻿
+namespace BlazorMovies.Server.Helpers
+{
+    public class FileStorageAzure : IStorageFiles
+    {
+        public Task DeleteFile(string path, string nameContainer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SaveFile(byte[] content, string extension, string nameContainer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
