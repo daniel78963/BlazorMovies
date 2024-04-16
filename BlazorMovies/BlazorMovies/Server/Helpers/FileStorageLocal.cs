2d167615b7362b61c6d78908aacaca442e138ee6
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-04-16 18:15:20 -0500

    SaveFile local

--- BlazorMovies/BlazorMovies/Server/Helpers/FileStorageLocal.cs
+++ BlazorMovies/BlazorMovies/Server/Helpers/FileStorageLocal.cs
@@ -17,9 +17,22 @@ namespace BlazorMovies.Server.Helpers
             throw new NotImplementedException();
         }
 
-        public Task<string> SaveFile(byte[] content, string extension, string nameContainer)
+        public async Task<string> SaveFile(byte[] content, string extension, string nameContainer)
         {
-            throw new NotImplementedException();
+            var nameFile = $"{Guid.NewGuid()}{extension}";
+            var folder = Path.Combine(env.WebRootPath, nameContainer);
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            string pathSave = Path.Combine(folder, nameFile);
+            await File.WriteAllBytesAsync(pathSave, content);
+
+            var urlActual = $"{httpContextAccessor!.HttpContext!.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host}";
+            var pathDB = Path.Combine(urlActual, nameContainer, nameFile);
+            return pathDB;
         }
     }
 }
