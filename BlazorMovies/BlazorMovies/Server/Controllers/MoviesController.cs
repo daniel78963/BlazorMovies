e869e3031f1eadfb2d8424578e05f1bb6fdb5cd0
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-05-11 12:02:09 -0500

    MoviesController post

--- BlazorMovies/BlazorMovies/Server/Controllers/MoviesController.cs
+++ BlazorMovies/BlazorMovies/Server/Controllers/MoviesController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlazorMovies.Server.Helpers;
+using BlazorMovies.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorMovies.Server.Controllers
 {
-    public class MoviesController : Controller
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext context;
+        private readonly IStorageFiles storageFiles;
+        private readonly string conteiner = "movies";
+
+        public MoviesController(ApplicationDbContext context, IStorageFiles storageFiles)
         {
-            return View();
+            this.context = context;
+            this.storageFiles = storageFiles;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Movie movie)
+        {
+            if (!string.IsNullOrWhiteSpace(movie.Poster))
+            {
+                //pasar de base64 a arreglo de bytes
+                var photoActor = Convert.FromBase64String(movie.Poster);
+                movie.Poster = await storageFiles.SaveFile(photoActor, ".jpg", conteiner);
+            }
+
+            context.Add(movie);
+            await context.SaveChangesAsync();
+            return movie.Id;
         }
     }
 }
