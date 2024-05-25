e106611b2f70de5916385ff4f83ec8da9510bdf1
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-05-25 11:20:15 -0500

    index Genders

--- BlazorMovies/BlazorMovies/Client/Repositories/IRepository.cs
+++ BlazorMovies/BlazorMovies/Client/Repositories/IRepository.cs
@@ -4,6 +4,7 @@ namespace BlazorMovies.Client.Repositories
 {
     public interface IRepository
     {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
         List<Movie> GetMovies();
         Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T send);
         Task<HttpResponseWrapper<TResponse>> PostAsync<T, TResponse>(string url, T send);
