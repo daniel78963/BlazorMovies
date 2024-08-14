ddefa11d3092dd60fe4593f14f75762dd1612016
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2024-08-13 21:15:32 -0500

    AutoMapper

--- BlazorMovies/BlazorMovies/Server/Program.cs
+++ BlazorMovies/BlazorMovies/Server/Program.cs
@@ -18,6 +18,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 builder.Services.AddTransient<IStorageFiles, FileStorageLocal>();
 builder.Services.AddHttpContextAccessor();
 
+//Automapper
+builder.Services.AddAutoMapper(typeof(Program));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
